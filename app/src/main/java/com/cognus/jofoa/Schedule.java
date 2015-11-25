7a8a59394fc96e17efc5b955ecf52d0184047e6e
Filipe Fusco <filipe.fusco@gmail.com>
2015-11-25 14:18:46 -0200

    Update to use TabLayout Google, stop jerking around with tabs!

--- app/src/main/java/com/cognus/jofoa/Schedule.java
+++ app/src/main/java/com/cognus/jofoa/Schedule.java
@@ -1,10 +1,12 @@
 package com.cognus.jofoa;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TableLayout;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -14,10 +16,9 @@ public class Schedule extends AppCompatActivity {
     AdView mAdView;
 
     private Toolbar mToolbar;
-
+    private TabLayout mTabLayout;
     private ViewPager mPager;
     private ViewPagerAdapter mViewPagerAdapter;
-    SlidingTabLayout mSlidingTabLayout;
     CharSequence Titles[]={"Segunda", "Terça", "Quarta", "Quinta", "Sexta"};
     int mNumberOfTAbs = 5;
 
@@ -29,19 +30,11 @@ public class Schedule extends AppCompatActivity {
         setSupportActionBar(mToolbar);
 
         mViewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), Titles,mNumberOfTAbs);
-        mPager = (ViewPager)findViewById(R.id.pager);
+        mPager = (ViewPager)findViewById(R.id.viewpager);
         mPager.setAdapter(mViewPagerAdapter);
 
-        mSlidingTabLayout = (SlidingTabLayout)findViewById(R.id.tabs);
-        mSlidingTabLayout.setDistributeEvenly(true);
-        mSlidingTabLayout.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
-            @Override
-            public int getIndicatorColor(int position) {
-                return ContextCompat.getColor(getApplicationContext(), R.color.green_dark);
-            }
-        });
-
-        mSlidingTabLayout.setViewPager(mPager);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(mPager);
 
         mAdView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().addTestDevice("CA6D1EFA27B8DABCBCE6C68C8F23B676").build();
